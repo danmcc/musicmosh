7b4b013d900a745ae2f952dc194d78377fb9fb7b
Dan McCormick <dan@codeop.com>
2014-10-07 22:42:03 -0400

    add validations to ShowImporter

--- lib/show_importer.rb
+++ lib/show_importer.rb
@@ -1,26 +1,48 @@
+require 'hash_validator'
+require 'countries'
+
 class ShowImporter
 
   def self.import(shows)
+    show_validation = {
+      artist_name: 'string',
+      venue: {
+        name:    'string',
+        city:    'string',
+        state:   lambda { |s| s.is_a?(String) && s.length == 2 },
+        country: lambda { |c| ! Country.new(c).nil? },
+      },
+      date: 'string'
+    }
+
+    errors = []
     shows.each do |show|
-      artist = Artist.find_or_create_by!(
-        name: show[:artist_name]
-      )
-      venue  = Venue.find_or_create_by!(
-        name: show[:venue][:name],
-        city: show[:venue][:city],
-        state: show[:venue][:state],
-        country: show[:venue][:country]
-      )
 
-      Show
-        .create_with( url: show[:url] )
-        .find_or_create_by!(
-        artist_id: artist.id,
-        venue_id:  venue.id,
-        date: show[:date]
-      )
+      validator = HashValidator.validate(show, show_validation)
+      if validator.valid?
+        artist = Artist.find_or_create_by!(
+          name: show[:artist_name]
+        )
+        venue  = Venue.find_or_create_by!(
+          name: show[:venue][:name],
+          city: show[:venue][:city],
+          state: show[:venue][:state],
+          country: show[:venue][:country]
+        )
 
+        Show
+          .create_with( url: show[:url] )
+          .find_or_create_by!(
+          artist_id: artist.id,
+          venue_id:  venue.id,
+          date: show[:date]
+        )
+      else
+        errors.push( validator.errors )
+      end
     end
+
+    return errors.any? ? errors : nil
   end
 
 end
