2db0dfe94ef601e976cd3d2fe81ed7fadd529eb9
Rui Gomes <ruigomeseu@gmail.com>
2015-01-07 15:24:26 +0000

    Added Time parser if Chronic fails

--- lib/kimono_helper.rb
+++ lib/kimono_helper.rb
@@ -4,42 +4,46 @@ require 'chronic'
 
 class KimonoHelper
 
-  def self.fetch(api_id)
-    url = "https://www.kimonolabs.com/api/#{api_id}?apikey=#{ENV['KIMONO_API_KEY']}"
-    page_data = ""
-    open(url) do |page|
-      page_data += page.read
-    end
+	def self.fetch(api_id)
+		url = "https://www.kimonolabs.com/api/#{api_id}?apikey=#{ENV['KIMONO_API_KEY']}"
+		page_data = ""
+		open(url) do |page|
+			page_data += page.read
+		end
 
-    return JSON.parse(page_data)
-  end
+		return JSON.parse(page_data)
+	end
 
-  def self.load_from_file(path) # for testing
-    page_data = File.read(path)
-    return JSON.parse(page_data)
-  end
+	def self.load_from_file(path) # for testing
+		page_data = File.read(path)
+		return JSON.parse(page_data)
+	end
 
-  def self.parse(data)
-    shows = []
+	def self.parse(data)
+		shows = []
 
-    data['results']['collection1'].each do |item|
-      artist_name = item['artist']['text']
-      url = item['artist']['href']
-      date = item['date']
+		data['results']['collection1'].each do |item|
+			artist_name = item['band']['text']
+			url = item['band']['href']
+			date = item['date']
 
-      if show_date.is_a?(Array)
-        show_date = show_date.grep(%r|/|).first # naively assume dates will have slashes
-      end
-      show_date = Chronic.parse(show_date)
+			if date.is_a?(Array)
+				show_date = Chronic.parse(date.first)
+			else
+          		show_date = Chronic.parse(date)
+				if show_date.nil?
+					show_date = Time.parse(date)
+				end
+			end
 
-      shows.push({
-        artist_name: artist_name,
-        url: url,
-        date: date,
-      })
-    end
+			shows.push({
+						   artist_name: artist_name,
+						   url: url,
+						   date: show_date,
+					   })
+		end
 
-    return shows
-  end
+		return shows
+	end
 
 end
