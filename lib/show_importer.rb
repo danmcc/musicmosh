7269d578cf97f70cd7cf0ef48fe730e2119bd23c
Rui Gomes <ruigomeseu@gmail.com>
2015-01-10 11:19:20 +0000

    Fixed importing duplicate shows

--- lib/show_importer.rb
+++ lib/show_importer.rb
@@ -12,17 +12,20 @@ class ShowImporter
         state:   lambda { |s| s.is_a?(String) && s.length == 2 },
         country: lambda { |c| ! Country.new(c).nil? },
       },
-      date: 'string'
+      date: 'time'
     }
 
     errors = []
+    imported_shows = 0
     shows.each do |show|
 
       validator = HashValidator.validate(show, show_validation)
       if validator.valid?
+
         artist = Artist.find_or_create_by!(
           name: show[:artist_name]
         )
+
         venue  = Venue.find_or_create_by!(
           name: show[:venue][:name],
           city: show[:venue][:city],
@@ -30,19 +33,28 @@ class ShowImporter
           country: show[:venue][:country]
         )
 
-        Show
-          .create_with( url: show[:url] )
-          .find_or_create_by!(
-          artist_id: artist.id,
-          venue_id:  venue.id,
-          date: show[:date]
-        )
+        conditions = {
+          :artist_id => artist.id,
+          :venue_id => venue.id,
+          :date => show[:date].to_date
+        }
+
+        unless Show.exists?(conditions)
+          Show.create(
+            artist_id: artist.id,
+            venue_id:  venue.id,
+            url: show[:url],
+            date: show[:date]
+          )
+          imported_shows += 1
+        end
+
       else
         errors.push( validator.errors )
       end
     end
 
-    return errors.any? ? errors : nil
+    return errors.any? ? [imported_shows, errors] : imported_shows
   end
 
 end
