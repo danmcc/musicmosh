19a595d6ad6dea06d280876f27c237c7b8e303ec
Rui Gomes <ruigomeseu@gmail.com>
2015-01-14 11:12:01 +0000

    Implemented a method to show a formatted date

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,8 @@
 class Show < ActiveRecord::Base
   belongs_to :artist
   belongs_to :venue
+
+  def show_date
+    date.strftime("%A, #{date.day.ordinalize} of %B %Y") + (time.nil? ? "" : time.strftime("%l:%M %p"))
+  end
 end
