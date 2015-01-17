aa9051e4e4c6621fa0c9d0daa4f8ce03e6d14eed
Dan McCormick <dan@codeop.com>
2015-01-17 14:15:46 -0500

    allow item['band'] to be an array

--- lib/kimono_helper.rb
+++ lib/kimono_helper.rb
@@ -4,46 +4,54 @@ require 'chronic'
 
 class KimonoHelper
 
-	def self.fetch(api_id)
-		url = "https://www.kimonolabs.com/api/#{api_id}?apikey=#{ENV['KIMONO_API_KEY']}"
-		page_data = ""
-		open(url) do |page|
-			page_data += page.read
-		end
+  def self.fetch(api_id)
+    url = "https://www.kimonolabs.com/api/#{api_id}?apikey=#{ENV['KIMONO_API_KEY']}"
+    page_data = ""
+    open(url) do |page|
+      page_data += page.read
+    end
 
-		return JSON.parse(page_data)
-	end
+    return JSON.parse(page_data)
+  end
 
-	def self.load_from_file(path) # for testing
-		page_data = File.read(path)
-		return JSON.parse(page_data)
-	end
+  def self.load_from_file(path) # for testing
+    page_data = File.read(path)
+    return JSON.parse(page_data)
+  end
 
-	def self.parse(data)
-		shows = []
+  def self.parse(data)
+    shows = []
 
-		data['results']['collection1'].each do |item|
-			artist_name = item['band']['text']
-			url = item['band']['href']
-			date = item['date']
+    data['results']['collection1'].each do |item|
+      date = item['date']
 
-			if date.is_a?(Array)
-				show_date = Chronic.parse(date.first)
-			else
-				show_date = Chronic.parse(date)
-				if show_date.nil?
-					show_date = Time.parse(date)
-				end
-			end
+      if date.is_a?(Array)
+        show_date = Chronic.parse(date.first)
+      else
+        show_date = Chronic.parse(date)
+        if show_date.nil?
+          show_date = Time.parse(date)
+        end
+      end
 
-			shows.push({
-						   artist_name: artist_name,
-						   url: url,
-						   date: show_date,
-					   })
-		end
+      if item['band'].is_a?(Array)
+        item['band'].each do |band|
+          shows << {
+            artist_name: band['text'],
+            url: band['href'],
+            date: show_date,
+          }
+        end
+      else
+        shows << {
+          artist_name: item['band']['text'],
+          url: item['band']['href'],
+          date: show_date,
+        }
+      end
+    end
 
-		return shows
-	end
+    return shows
+  end
 
 end
