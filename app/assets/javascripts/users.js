a8d9088a21540dbc31c98209930fc634695f8341
Rui Gomes <ruigomeseu@gmail.com>
2015-01-21 20:31:49 +0000

    Landing page redirects to the right endpoint
    Still need to implement getting the country and state if the user
    manually inputs the city instead of using geolocation

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -28,23 +28,54 @@ $(document).ready(function() {
                 if (status == google.maps.GeocoderStatus.OK) {
                     //Check result 0
                     var result = results[0];
+                    console.log(results);
                     //look for locality tag and administrative_area_level_1
                     var city = "";
                     var state = "";
+                    var country = "";
                     for(var i=0, len=result.address_components.length; i<len; i++) {
                         var ac = result.address_components[i];
                         if(ac.types.indexOf("locality") >= 0) city = ac.long_name;
                         if(ac.types.indexOf("administrative_area_level_1") >= 0) state = ac.long_name;
+                        if(ac.types.indexOf("country") >= 0) country = ac.short_name;
                     }
                     //only report if we got Good Stuff
                     if(city != '' && state != '') {
-                        $("#query").val(city);
+                        $("#city").val(city);
+                        $("#state").val(state);
+                        $("#country").val(country);
                     }
                 }
                 spinner.stop();
             });
 
         });
+
+        $("#city").focus();
+
+    });
+
+    var showsForm = $("#shows-form");
+
+    $("#city").focus();
+
+    showsForm.submit(function(e) {
+        e.preventDefault();
+
+        var city = $("#city").val();
+        var state = $("#state").val();
+        var country = $("#country").val();
+
+        city = encodeURIComponent(city).toLowerCase();
+        state = encodeURIComponent(state).toLowerCase();
+
+        var baseUrl = showsForm.attr('action');
+
+        var completeUrl = baseUrl + '/' + country + '/' + state + '/' + city;
+
+        window.location.href = completeUrl;
+
+        console.log(baseUrl + '/' + country + '/' + state + '/' + city);
 
 
     });
