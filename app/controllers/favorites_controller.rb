0276a89e0f88ee8026158fd3e958a25bd5496c6d
Rui Gomes <ruigomeseu@gmail.com>
2015-01-30 14:25:17 +0000

    Create method on Favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,10 +5,32 @@ class FavoritesController < ApplicationController
 	end
 
 	def create
+
+    if current_user.nil?
+      output = {'login' => 0}
+
+      return respond_to do |format|
+        format.json {
+          render :json => output, :status => 401
+        }
+      end
+
+    end
+
+    favorite = Favorite.new(
+      user_id: current_user.id,
+      show_id: params['show_id']
+    )
+
+    if favorite.save
+      output = {'success' => 1}.to_json
+    else
+      output = {'success' => 0}.to_json
+    end
+
 		respond_to do |format|
-			format.html {render text: "Your data was sucessfully loaded. Thanks"}
 			format.json {
-				render text: params.to_json
+				render :json => output
 			}
 		end
 	end
