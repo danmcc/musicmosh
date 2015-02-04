d102f51aaccfaedcda30baa727d23f4fcd31cbe4
Rui Gomes <ruigomeseu@gmail.com>
2015-02-04 17:08:03 +0000

    Added validation rules to favorite model

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,6 +2,9 @@ class Favorite < ActiveRecord::Base
   belongs_to :show
   belongs_to :user
 
+  validates :show, presence: true
+  validates :user, presence: true
+
   validates_uniqueness_of :show_id, :scope => :user
 
 end
