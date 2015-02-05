076c050295584e621505ffa536acd30c8eb8d906
Rui Gomes <ruigomeseu@gmail.com>
2015-02-05 16:44:51 +0000

    Added name requirement for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
 
   has_many :favorites
 
+  validates :name, presence: true
+
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
