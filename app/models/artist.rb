9d9786f86460fd02aab08dc484da4a09f9f551a0
Rui Gomes <ruigomeseu@gmail.com>
2015-02-10 18:18:17 +0000

    "TBA" shouldn't be a valid artist name

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,7 @@
 class Artist < ActiveRecord::Base
   has_many :shows
 
-  validates :name, presence: true
+  validates :name, presence: true, exclusion:  %w(tba TBA Tba)
 
   def encoded_name
     CGI.escape(self.name)
