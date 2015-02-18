90e9dd85355b88eafea7f7f3bec85f3ece74904f
Rui Gomes <ruigomeseu@gmail.com>
2015-02-18 17:50:09 +0000

    Added location dropdown on landing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
 
   def index
+    venues = Venue.select(:city, :state, :country).distinct
 
+    @locations = []
+
+    venues.each do |venue|
+      @locations << {
+        :name => venue.city + ', ' + venue.state + ', ' + venue.country,
+        :url => show_location_path(venue.country, venue.state, CGI.escape(venue.city))
+      }
+    end
   end
 
 end
