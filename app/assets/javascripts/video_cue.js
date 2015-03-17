cdbd21b3ebfd698c67004faee6f9663e20986926
Rui Gomes <ruigomeseu@gmail.com>
2015-03-17 11:06:14 +0000

    The Youtube player now works on mobile devices

--- app/assets/javascripts/video_cue.js
+++ app/assets/javascripts/video_cue.js
@@ -1,22 +1,21 @@
-function onYouTubePlayerReady(playerId) {
+function cuePlaylist(event) {
 
-    ytplayer = document.getElementById("artist-playlist-yt-player");
+  event.target.cuePlaylist(artistVideoIDs);
 
-    ytplayer.cuePlaylist(artistVideoIDs);
-
-    ytplayer.playVideo();
-
-    ytplayer.addEventListener("onStateChange", "nextArtist");
-
+  event.target.addEventListener("onStateChange", "nextArtist");
 
 }
 
 function nextArtist(event) {
 
-    //If the playlist has ended, redirect to the next artist
-    if(event == 0)
-    {
-        document.location.href = document.getElementById("next-show-link").getAttribute('href', 2);
-    }
+  //If the playlist has ended, redirect to the next artist
+  if (event.data == 0) {
+    document.location.href = document.getElementById("next-show-link").getAttribute('href', 2);
+  }
+
+  //When the video is cued and loaded, start playing
+  if (event.data == 5) {
+    event.target.playVideo();
+  }
 
 }
