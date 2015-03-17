55a5766c19ab2eb37814442d65a3405b1fc05a91
Rui Gomes <ruigomeseu@gmail.com>
2015-03-17 11:45:44 +0000

    Refactored JS mobile detection

--- app/assets/javascripts/video_cue.js
+++ app/assets/javascripts/video_cue.js
@@ -1,3 +1,8 @@
+function isMobile()
+{
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
 function cuePlaylist(event) {
 
   event.target.cuePlaylist(artistVideoIDs);
@@ -9,13 +14,13 @@ function cuePlaylist(event) {
 function nextArtist(event) {
 
   //If the playlist has ended, redirect to the next artist
-  if (event.data == 0) {
+  if (event.data == 0 && !isMobile()) {
     document.location.href = document.getElementById("next-show-link").getAttribute('href', 2);
   }
 
   //If the video has loaded and the user is not mobile
   //start playing
-  if (event.data == 5 && !(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+  if (event.data == 5 && !isMobile()) {
     console.log('melo');
     event.target.playVideo();
   }
